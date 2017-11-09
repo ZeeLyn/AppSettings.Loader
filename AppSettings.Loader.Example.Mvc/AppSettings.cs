83de7ff026a34855fdd9dc97bd14b8d1f76f767f
wangzelin <315872858@qq.com>
2017-11-09 15:34:01 +0800

    eidt Example

---
+++ AppSettings.Loader.Example.Mvc/AppSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace AppSettings.Loader.Example.Mvc
+{
+	public class AppSettings
+	{
+		[JsonIgnore]
+		public static AppSettings Get => SettingsLoader<AppSettings>.Get();
+
+
+		public static void Rest()
+		{
+			SettingsLoader<AppSettings>.Rest();
+		}
+
+		public string Version { get; set; }
+	}
+}
