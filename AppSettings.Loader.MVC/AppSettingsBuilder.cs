e0e65e332faa88883b772e15517ca6e17650f061
wangzelin <315872858@qq.com>
2018-07-31 11:41:38 +0800

    fix netcore 2.1

--- AppSettings.Loader.MVC/AppSettingsBuilder.cs
+++ AppSettings.Loader.MVC/AppSettingsBuilder.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace AppSettings.Loader.MVC
 {
@@ -13,6 +15,30 @@ namespace AppSettings.Loader.MVC
 		public static void AddAppSettingsJsonFile(this IApplicationBuilder builder, string jsonfilepath, bool reloadOnChange = true)
 		{
 			AppSettingsRegister.BindJsonFile(jsonfilepath, reloadOnChange);
+		}
+
+		/// <summary>
+		/// 设置配置文件
+		/// </summary>
+		/// <param name="builder"></param>
+		/// <param name="jsonfilepath">json文件路径</param>
+		/// <param name="reloadOnChange">文件被修改时是否自动重新加载</param>
+		public static IConfiguration AddAppSettingsJsonFile(this IConfiguration builder, string jsonfilepath, bool reloadOnChange = true)
+		{
+			AppSettingsRegister.BindJsonFile(jsonfilepath, reloadOnChange);
+			return builder;
+		}
+
+		/// <summary>
+		/// 设置配置文件
+		/// </summary>
+		/// <param name="builder"></param>
+		/// <param name="jsonfilepath">json文件路径</param>
+		/// <param name="reloadOnChange">文件被修改时是否自动重新加载</param>
+		public static IServiceCollection AddAppSettingsJsonFile(this IServiceCollection builder, string jsonfilepath, bool reloadOnChange = true)
+		{
+			AppSettingsRegister.BindJsonFile(jsonfilepath, reloadOnChange);
+			return builder;
 		}
 	}
 }
